2294fbae0b23b36df9a8d737f848f7b38bee1f54
Luv Gupta <lg5122003@gmail.com>
2023-11-12 20:43:45 +0530

    fix: images in Testimonials

--- src/sections/data.jsx
+++ src/sections/data.jsx
@@ -1,31 +1,36 @@
+import Namita from '../../public/namita.png'
+import Bharti from "../../public/bharti.png"
+import Raja from "../../public/rajalingam.png"
+import Kuldeep from "../../public/kuldeep.png"
+import Pranati from "../../public/pranati.png"
 export const DATA = [
   {
     id: 0,
-    image: "../../public/pranati.png",
+    image: Pranati,
     name: "Pranati Tiwari",
     desc: "I’m a 2nd Year BTech Student of Jaypee Institute of information technology, Noida. I participated as a Campus Ambassador of the first online edition of Apogee,Techfest of Bits Pilani. It helped me gain the experience of working as a team in promoting the event all over the social media platforms and build my resume.",
   },
   {
     id: 1,
-    image: "../../public/kuldeep.png",
+    image: Kuldeep,
     name: "Kuldeep Dwivedi",
     desc: "I really enjoyed working as a Campus Ambassador for APOGEE BITS Pilani. Working with a great team of leaders and fellow students on varied events made each day different and exciting. As well as it being a great way to meet new people and develop yourself as a person. Becoming a Campus Ambassador really helped my confidence and I definitely learned a lot along the way.",
   },
   {
     id: 2,
-    image: "../../public/rajalingam.png",
+    image: Raja,
     name: "Rajalingam M",
     desc: "This will be definitely a great internship for who needs to improve their outreach, professional communication and networking skills. Moreover, even in this pandemic and remote working, it again made it's wonderful tech festival to great height without fail.",
   },
   {
     id: 3,
-    image: "../../public/bharti.png",
+    image: Bharti,
     name: "Bharti Kumari",
     desc: "I began working as a Campus Ambassador (CA) when I was in Third year. I first worked an Open Day and after that, I was hooked! The work is varied which keeps it interesting, from campus tours to sign in desks and setting up for events, it helped me develop lots of skills which I'm sure I will use in my future career. It's also great that you can fit the work in around your college schedule and make use of hours off in college to earn some extra skills! The work was always good fun because you were with other people.",
   },
   {
     id: 4,
-    image: "../../public/namita.png",
+    image: Namita,
     name: "Namita Tupkar",
     desc: " My experience being a campus ambassador of APOGEE was pretty amazing. The events were very nicely organised even though they were held virtually. It was a very good experience wherein the day to day activities and tasks were well carried out. The internship offered and gave me such a wonderful time that I'll always cherish. The management of the entire event was really nice.",
   },
