e7c09e2260bacecf4a2b855dab410039476e58df
Luv Gupta <lg5122003@gmail.com>
2023-11-19 22:40:49 +0530

    feat: add Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,10 @@ import { motion } from "framer-motion";
 import FAQs from "./sections/FAQs";
 import MobilePerks from "./sections/MobilePerks";
 
+import ReactGa from "react-ga4";
+
 function App() {
+  ReactGa.initialize("G-4J7ZQFX5E8");
   // useref for gsap observer
   const mainObserver = useRef(null);
   const [isFormOpen, setIsFormOpen] = useState(false);
@@ -25,7 +28,7 @@ function App() {
     mainObserver.current = gsapInit(isFormOpen);
 
     return () => {
-      if (mainObserver.current !== null){
+      if (mainObserver.current !== null) {
         mainObserver.current.kill();
       }
     };
@@ -41,8 +44,20 @@ function App() {
       <Navbar />
       <Home isFormOpen={isFormOpen} setIsFormOpen={setIsFormOpen} />
       <About />
-      {typeof window !== "undefined" ? window.innerWidth > 1100 ? <Perks /> : <MobilePerks section="perks" /> : null}
-      {typeof window !== "undefined" ? window.innerWidth > 1100 ? <Responsibility /> : <MobilePerks section="responsibility" />: null}
+      {typeof window !== "undefined" ? (
+        window.innerWidth > 1100 ? (
+          <Perks />
+        ) : (
+          <MobilePerks section="perks" />
+        )
+      ) : null}
+      {typeof window !== "undefined" ? (
+        window.innerWidth > 1100 ? (
+          <Responsibility />
+        ) : (
+          <MobilePerks section="responsibility" />
+        )
+      ) : null}
       <Testimonials />
       <FAQs />
       <Contact />
